6b85cc2913774f141bc21631efdaed002a4dbd4b
RaviVarma28 <gedela.ravi28@gmail.com>
2024-12-20 15:01:41 +0530

    Adding Streamlit files

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import streamlit as st
 import pandas as pd
+import sys
+sys.path.append('scripts')
+import Preprocessor
 
-st.title("Air Quality Index")
-df = pd.read_csv('data/CleanedAirQuality.csv')
+st.logo("static/Logo.jpg")
 
-st.markdown('**Air Quality Dataset**, [*via kaggle*](https://www.kaggle.com/datasets/fedesoriano/air-quality-data-set)')
+col1, col2 = st.columns([1,3])
 
-st.text('Sample Dataset:')
-st.dataframe(df.head())
+with col2:
+    st.image("static/Logo.jpg", width=120)
 
-st.sidebar.subheader("Select")
-pollutant_cols = st.multiselect("Pollutants")
+with col1:
+    st.markdown("# Air Quality Index")
+    st.markdown('**Air Quality Dataset**, [*via kaggle*](https://www.kaggle.com/datasets/fedesoriano/air-quality-data-set)')
+
+
+st.subheader('Data Preview:')
+st.write(Preprocessor.df.head(), width=1000)
+
+st.subheader("Data Summary:")
+st.write(Preprocessor.df.head())
+
+# st.subheader("Highest recorded Pollution level")
+pollutant = st.selectbox("**Select the Pollutant**",Preprocessor.pollutant_cols)
+if pollutant:
+    peak = Preprocessor.df[pollutant].max()
+    peak_date = Preprocessor.df[Preprocessor.df[pollutant]==peak]['Date'].values[0]
+    st.metric(label = 'Highest recorded Pollution level',value=f'{peak} mg/m³ at {peak_date}')
+
+ 
