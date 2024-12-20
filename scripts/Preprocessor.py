afeda3434c409addf596b3a78c2d8d1b78b22944
RaviVarma28 <gedela.ravi28@gmail.com>
2024-12-20 15:04:55 +0530

    small modification

--- scripts/Preprocessor.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data/CleanedAirQuality.csv')
-
-
-sensor_cols = ['PT08.S1(CO)', 'PT08.S3(NOx)',  'PT08.S4(NO2)', 'PT08.S2(NMHC)']
-pollutant_cols = ['CO(GT)', 'NOx(GT)', 'NO2(GT)', 'NMHC(GT)']
-weather_cols = ['T', 'RH', 'AH']
